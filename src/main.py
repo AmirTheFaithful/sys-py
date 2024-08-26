6f64db0c2089af912bad779887036395f04c8ed4
Amir Daneshmand <amirdanesh02@gmail.com>
2024-08-27 02:28:09 +0330

    First commit

---
+++ src/main.py
@@ -0,0 +1,5 @@
+def main() -> None:
+  print("Hi!")
+
+if __name__ == "__main__":
+  main()
---
+++ src/units/unit.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+from uuid import uuid4
+from typing import Any
+
+@dataclass
+class Port:
+  id: str
+  in_use: bool
+
+# Data Transport Unit
+class DTU:
+  _store: dict[str, Port]
+
+  def __init__(self):
+    self._store = {}
+
+class Unit:
+  _ports: dict[str, Port] = {}
+  # For now, the storage will be just a dict
+  _storage: dict[str, Any] = {}
+  status: str = "public"
+
+  def __init__(self):
+    # First generate ports:
+    self._gen_ports()
+
+  def _gen_ports(self) -> None:
+    if self._ports:
+      return
+    
+    # Initialize three ports one by one
+    for index in range(1, 4):
+      port_id: str = f"U#{index}:{uuid4()}"
+      port: Port = Port(id=port_id, in_use=False)
+      self._ports[port_id] = port
+  
+  # Log short info about each port
+  def log_ports(self) -> None:
+    for id, port in self._ports.items():
+      print(f"{id}: {port.in_use}")
