b71a3a0a6be3124d239dbd2c88c67c5c2d94d5ac
Amir Daneshmand <amirdanesh02@gmail.com>
2024-09-01 17:45:55 +0330

    Add erro handler class

--- src/env/exceptions.py
+++ src/env/exceptions.py
@@ -4,21 +4,31 @@ class ErrorCodes(Enum):
   NO_FREE_PORTS = 500
   STRG_DPLCT_DT = 501
 
+class ExceptionHandler:
+  _exception: Exception
+  def __init__(self, exception: Exception):
+    self._exception = exception
+  
+  def log_exc(self) -> None:
+    print(str(self._exception))
+
 class NoFreePorts(Exception):
   message: str
 
-  def __init__(self, msg: str, err_code: int):
+  def __init__(self, msg: str):
     self.message = msg
-    self.err_code = err_code
+    self.err_code = ErrorCodes.NO_FREE_PORTS
+    self.name = str(ErrorCodes(self.err_code))
     super().__init__(self.message)
 
   def __str__(self):
-    return f"{self.message} (Error code: {self.err_code})"
+    return f"\nName: {self.name}\n Message: {self.message}\n Error code: {self.err_code})\n"
 
 class StorageDuplicateData(Exception):
   massage: str
 
-  def __init__(self, msg: str, err_code: int):
+  def __init__(self, msg: str):
     self.message = msg
-    self.err_code = err_code
+    self.err_code = ErrorCodes.STRG_DPLCT_DT
+    self.name = str(ErrorCodes(self.err_code))
     super().__init__(self.message)
